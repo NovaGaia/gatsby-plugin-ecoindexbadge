6bc86e4975636f19a7808e8a150bb6ca81548ecf
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-03-16 14:57:12 +0100

    add comment to gatsby-browser.js

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,3 +1,8 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
+ */
 import React from 'react'
 
 exports.onRouteUpdate = pluginOptions => {
